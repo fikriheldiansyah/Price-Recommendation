2c033f29f602d1b69b04616e8a28d996fdd279b1
fixya <fikri_heldiansyah@yahoo.co.id>
2019-11-18 22:25:50 +0700

    make login form

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="/css/styles1.css">
+
+    <title>Price Recommendation</title>
+  </head>
+  <body>
+    <div class="login-box">
+    <h1>Login</h1>
+    <div class ="textbox">
+    <i class="fas fa-user-tie" aria-hidden="true"></i>
+    <input type="text" placeholder="Username" name="" value="">
+    </div>
+    <div class ="textbox">
+    <i class="fas fa-lock"></i>
+    <input type="password" placeholder="Password" name="" value="">
+    </div>
+    <a href="/welcome">
+        <input class="btn" type="button" value ="Sign In"> 
+    </a>
+    <input class="btn" type="button" value ="Sign In"> 
+  </body>
+
+</html>
